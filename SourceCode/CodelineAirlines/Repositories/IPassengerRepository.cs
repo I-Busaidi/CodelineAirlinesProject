d23a1c2b0157c349218f4c93c015cde76ac96642
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-29 16:03:31 +0400

    Add Register As Passenger

---
+++ SourceCode/CodelineAirlines/Repositories/IPassengerRepository.cs
@@ -0,0 +1,10 @@
+﻿using CodelineAirlines.Models;
+
+namespace CodelineAirlines.Repositories
+{
+    public interface IPassengerRepository
+    {
+        void AddPassenger(Passenger passenger);
+        bool PassengerExistsForUser(int userId);
+    }
+}
---
+++ SourceCode/CodelineAirlines/Repositories/PassengerRepository.cs
@@ -0,0 +1,25 @@
+﻿using CodelineAirlines.Models;
+
+namespace CodelineAirlines.Repositories
+{
+    public class PassengerRepository : IPassengerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PassengerRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddPassenger(Passenger passenger)
+        {
+            _context.Passengers.Add(passenger);
+            _context.SaveChanges();
+        }
+        public bool PassengerExistsForUser(int userId)
+        {
+            return _context.Passengers.Any(p => p.UserId == userId);
+        }
+
+    }
+}
