649480db80775f198c70166f4762299602c2f73b
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-29 08:53:29 +0400

    Add Register user Methods in user repo

---
+++ SourceCode/CodelineAirlines/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using CodelineAirlines.Models;
+
+namespace CodelineAirlines.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public int AddUser(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return user.UserId; // Return the newly created user's ID
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.InnerException.Message);
+            }
+        }
+    }
+}
