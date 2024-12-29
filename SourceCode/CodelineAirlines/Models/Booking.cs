d74aac599f2b1dd941cc1c56cf522c5358ccbc51
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-12-29 08:31:05 +0400

    adding project files.

---
+++ SourceCode/CodelineAirlines/Models/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodelineAirlines.Models
+{
+    public class Booking
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookingId { get; set; }
+
+        [Required]
+        [ForeignKey("Passenger")]
+        public string PassengerPassport { get; set; }
+
+        [Required]
+        [ForeignKey("Flight")]
+        public int FlightNo { get; set; }
+
+        public string? SeatNo { get; set; }
+
+        [Required]
+        public decimal TotalCost { get; set; }
+        public string? Meal { get; set; }
+        public int Status { get; set; } = 0;
+        public DateTime BookingDate { get; set; } = DateTime.Now;
+
+        public Passenger Passenger { get; set; }
+        public Flight Flight { get; set; }
+    }
+}
---
+++ SourceCode/CodelineAirlines/Models/SeatTemplate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodelineAirlines.Models
+{
+    [PrimaryKey(nameof(AirplaneModel), nameof(SeatNumber))]
+    public class SeatTemplate
+    {
+        [Required(ErrorMessage = "Airplane model is required")]
+        public string AirplaneModel { get; set; }
+
+        [Required(ErrorMessage = "Seat type is required")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Seat number is required")]
+        public string SeatNumber { get; set; }
+
+        public bool IsWindowSeat { get; set; } = false;
+
+        public decimal SeatCost { get; set; } = 0;
+    }
+}
