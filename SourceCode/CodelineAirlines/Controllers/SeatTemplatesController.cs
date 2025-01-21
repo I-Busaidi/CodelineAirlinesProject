fe16e709a45f65d2ad555d33aa485da95ae94f74
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2025-01-21 08:33:19 +0400

    updating database and removing 2 api from seatTemplateController.

--- SourceCode/CodelineAirlines/Controllers/SeatTemplatesController.cs
+++ SourceCode/CodelineAirlines/Controllers/SeatTemplatesController.cs
@@ -21,27 +21,27 @@ namespace CodelineAirlines.Controllers
         }
 
         // Endpoint to generate seat templates for an airplane model
-        [Authorize(Roles = "admin")]
-        [HttpPost("generate")]
-        public IActionResult GenerateSeatTemplates([FromBody] GenerateSeatTemplateDto dto)
-        {
-            try
-            {
-                if (dto == null || string.IsNullOrEmpty(dto.AirplaneModel))
-                {
-                    return BadRequest("Invalid data.");
-                }
+        //[Authorize(Roles = "admin")]
+        //[HttpPost("generate")]
+        //public IActionResult GenerateSeatTemplates([FromBody] GenerateSeatTemplateDto dto)
+        //{
+        //    try
+        //    {
+        //        if (dto == null || string.IsNullOrEmpty(dto.AirplaneModel))
+        //        {
+        //            return BadRequest("Invalid data.");
+        //        }
 
-                // Call the service layer to generate seat templates
-                _seatTemplateService.GenerateSeatTemplatesForModel(dto);
+        //        // Call the service layer to generate seat templates
+        //        _seatTemplateService.GenerateSeatTemplatesForModel(dto);
 
-                return Ok("Seat templates successfully generated.");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //        return Ok("Seat templates successfully generated.");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
         // Endpoint to get seat templates by airplane model, ordered by SeatCost in descending order
         [Authorize(Roles = "admin")]
@@ -61,16 +61,16 @@ namespace CodelineAirlines.Controllers
         }
 
         // Endpoint to delete seat templates by airplane model
-        [Authorize(Roles = "admin")]
-        [HttpDelete("delete/{airplaneModel}")]
-        public IActionResult DeleteSeatTemplatesByModel(string airplaneModel)
-        {
-            // Call the service to delete the seat templates by model
-            _seatTemplateService.DeleteSeatTemplatesByModel(airplaneModel);
+        //[Authorize(Roles = "admin")]
+        //[HttpDelete("delete/{airplaneModel}")]
+        //public IActionResult DeleteSeatTemplatesByModel(string airplaneModel)
+        //{
+        //    // Call the service to delete the seat templates by model
+        //    _seatTemplateService.DeleteSeatTemplatesByModel(airplaneModel);
 
-            // Return a success response
-            return NoContent(); // HTTP 204 No Content to indicate successful deletion
-        }
+        //    // Return a success response
+        //    return NoContent(); // HTTP 204 No Content to indicate successful deletion
+        //}
 
         [AllowAnonymous]
         [HttpGet("ViewAvailableModels")]
